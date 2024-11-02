7e549f73a04075d6efa462f6007a620d86490e3f
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-11-02 12:17:32 -0300

    Inserindo as funcionalidades de hook e components

---
+++ src/components/textInput.jsx
@@ -0,0 +1,18 @@
+
+
+export default function TextInput({ id, label, value, setValue}){
+    
+
+    return (
+        <div>
+            <label htmlFor={id}>{label}</label>
+            <input 
+                type="text" 
+                name={id}
+                id={id} 
+                value={value} 
+                onChange={(ev) => setValue(ev.target.value)} 
+            />
+        </div>
+    )
+}
